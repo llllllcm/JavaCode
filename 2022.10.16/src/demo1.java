a33c961f48a9c1cf5723368630fbef98abb85f62
林铖敏 <10316873+lin-cmin@user.noreply.gitee.com>
2022-10-18 11:40:13 +0800

    学校实验

--- 2022.10.16/src/demo1.java
+++ 2022.10.16/src/demo1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -6,30 +8,43 @@
  * Time: 20:59
  */
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        //规定dp[m-1][n-1] 是到这个地方的最小路径和
-        //dp[i][j] = arr[i][j] + Math(dp[i-1][j],dp[i][j-1])
-        //给定初始值
-        dp[0][0] = arr[0][0];
-        //然后呢 又因为这里的i或j都不能为0，如果为0，那么后面数组就会访问越界
-        //所以分别对最左行，和第一行进行初始化
-
-        //对第一行初始化
-        for(int i = 1; i < m; i++) {
-            dp[i][0] = arr[i][0] + dp[i-1][0];
-        }
-        //对最左列进行初始化
-        for(int j = 1; j < n; i++) {
-            dp[0][j] = arr[0][j] + dp[0][j-1];
-        }
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                dp[i-1][j-1] = Math(dp[i][j-1],dp[i-1][j])+arr[i-1][j-1];
-            }
-        }
-        return dp[i-1][j-1];
+    public static void Swap(int[] arr) {
+        int tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
     }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2};
+        System.out.println(Arrays.toString(arr));
+        Swap(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+//    public int minPathSum(int[][] grid) {
+//        int m = grid.length;
+//        int n = grid[0].length;
+//        int[][] dp = new int[m][n];
+//        //规定dp[m-1][n-1] 是到这个地方的最小路径和
+//        //dp[i][j] = arr[i][j] + Math(dp[i-1][j],dp[i][j-1])
+//        //给定初始值
+//        dp[0][0] = arr[0][0];
+//        //然后呢 又因为这里的i或j都不能为0，如果为0，那么后面数组就会访问越界
+//        //所以分别对最左行，和第一行进行初始化
+//
+//        //对第一行初始化
+//        for(int i = 1; i < m; i++) {
+//            dp[i][0] = arr[i][0] + dp[i-1][0];
+//        }
+//        //对最左列进行初始化
+//        for(int j = 1; j < n; i++) {
+//            dp[0][j] = arr[0][j] + dp[0][j-1];
+//        }
+//        for(int i = 1; i < m; i++) {
+//            for(int j = 1; j < n; j++) {
+//                dp[i-1][j-1] = Math(dp[i][j-1],dp[i-1][j])+arr[i-1][j-1];
+//            }
+//        }
+//        return dp[i-1][j-1];
+//    }
 }
--- 2022.10.16/src/leetCode/Main.java
+++ 2022.10.16/src/leetCode/Main.java
@@ -37,29 +37,97 @@ package leetCode;
 import java.util.Scanner;
 
 // 注意类名必须为 Main, 不要有任何 package xxx 信息
+class Teacher{
+    int id;  //学号
+    String name;  //姓名
+    int age;  //年龄
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
 public class Main {
+    public static void Swap(int[] arr) {
+        int tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
+
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // 注意 hasNext 和 hasNextLine 的区别
         String str = in.nextLine();
-        //定义两个字符串，用来比较的，s2则用来存放答案。
-        StringBuffer s1 = new StringBuffer();
-        StringBuffer s2 = new StringBuffer();
-        System.out.println(s1.length());
-        s1.reverse();
-        s2.insert(1,s2);
-        for(int i = 0; i < str.length(); i++ ) {
-            if(str.charAt(i) >= '0' && str.charAt(i) <= '9') {
-                //遇见这个数字串
-                s1.append(str.charAt(i));
-                s1.append('a');
-            }else {
-                if(s1.length() > s2.length()) {
-                    s2 = s1;
-                }
-
+        String[] arr = str.split(" ");
+        for(int i = 0; i < arr.length; i++ ) {
+            System.out.println(arr[i]);
+        }
+        int[] arrInt = new int[arr.length];
+        for(int i = 0; i < arr.length; i++ ) {
+            arrInt[i] = Integer.parseInt(arr[i]);
+        }
+        //找出最大的元素
+        int max = 0;
+        int maxIndex = 0;
+        for(int i = 0; i < arrInt.length; i++ ) {
+            if(arrInt[i] > max) {
+                max = arrInt[i];
+                maxIndex = i;
             }
+        }
+        int min = 0;
+        int minIndex = 0;
+        //找出最小的元素
+        for(int i = 0; i < arrInt.length; i++ ) {
+            if(arrInt[i] < min) {
+                min = arrInt[i];
+                minIndex = i;
+            }
+        }
+        int[] swapMax = {arrInt[0],arrInt[maxIndex]};
+        int[] swapMin = {arrInt[arrInt.length-1],arrInt[minIndex]};
+        //写一个交换的函数
+        Swap(swapMin);
+        Swap(swapMax);
+        for(int i = 0; i < arrInt.length; i++) {
+            System.out.println(arrInt[i]);
         }
     }
 }
+//public class Main {
+
+
+//    public static void main(String[] args) {
+//        Teacher teacher = new Teacher();
+//        System.out.println(teacher.toString());
+//    }
+//    public static void main1(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int[] arr = new int[]
+//        // 注意 hasNext 和 hasNextLine 的区别
+//        String str = in.nextLine();
+//        //定义两个字符串，用来比较的，s2则用来存放答案。
+//        StringBuffer s1 = new StringBuffer();
+//        StringBuffer s2 = new StringBuffer();
+//        System.out.println(s1.length());
+//        s1.reverse();
+//        s2.insert(1,s2);
+//        for(int i = 0; i < str.length(); i++ ) {
+//            if(str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+//                //遇见这个数字串
+//                s1.append(str.charAt(i));
+//                s1.append('a');
+//            }else {
+//                if(s1.length() > s2.length()) {
+//                    s2 = s1;
+//                }
+//
+//            }
+//        }
+//    }
+//}
 
