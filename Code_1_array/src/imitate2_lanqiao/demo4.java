60c9df13253aa869c3dd1506bda213d775ca2045
林铖敏 <10316873+lin-cmin@user.noreply.gitee.com>
2022-12-05 23:36:51 +0800

    lanqiao

---
+++ Code_1_array/src/imitate2_lanqiao/demo4.java
@@ -0,0 +1,15 @@
+package imitate2_lanqiao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 86136
+ * Date: 2022-12-05
+ * Time: 20:55
+ */
+public class demo4 {
+    public static void main(String[] args) {
+        String s = String.valueOf(2);
+        Integer.parseInt(s,16);
+    }
+}
